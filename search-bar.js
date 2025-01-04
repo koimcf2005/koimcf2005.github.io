8c50302ee7634287788220b3eaec4d79257af18c
koimcf2005 <koimcf168@gmail.com>
2025-01-03 20:19:57 -0500

    Added Fuel

--- search-bar.js
+++ search-bar.js
@@ -6,14 +6,14 @@ document.addEventListener('DOMContentLoaded', function () {
 
     const games = {
         'Breath_Of_The_Wild': { width: 9.79, height: 8.16, area: 49.36 },
-        'Elden_Ring': { width: 9.107, height: 8.6625, area: 78.85 },
-        'Elden_Ring_(No_Ocean)': { width: 9.107, height: 8.6625, area: 26.66 },
+        'Elden_Ring': { width: 9.107, height: 8.6625, area: 26.66 },
         'Elden_Ring_(Playable_Area)': { width: 9.107, height: 8.6625, area: 13.65 },
         'Rocket_League': { width: 0.274, height: 0.396, area: 0.11 },
         'Fortnite_(Season_1)': { width: 2.588, height: 2.588, area: 4.28 },
         'Grand_Theft_Auto_III': { width: 3.39, height: 2.41, area: 3.78 },
         'Grand_Theft_Auto_V': { width: 7.68, height: 10.43, area: 37.36 },
         'Skyrim': { width: 7.66, height: 4.77, area: 19.3 },
+        'Fuel': { width: 120.0, height: 120.0, area: 14400.0 },
     };
 
     function updateGameList(filter = '') {
